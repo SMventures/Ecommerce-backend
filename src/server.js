1c57fa62206c40c9ab895665490ddf6e2e5167fe
kanchana37 <kanchanachopra376@gmail.com>
2024-02-19 13:51:25 +0530

    installed few dependencies

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ const app = require(".");
 const {connectDb} = require('./config/db');
 
 
-const PORT=5454;
-app.listen(PORT,async()=>{
-    await  connectDb();
-    console.log("ecommerce api listening on :", PORT);
-})
+const PORT = process.env.PORT || 5454; // Use environment variable or default to 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
